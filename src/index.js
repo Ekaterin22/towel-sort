a863b6b20df036377320c311720a00efb938b7d9
Ekaterin22 <77670720+Ekaterin22@users.noreply.github.com>
2021-03-09 23:58:51 +0400

    feat: implement towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == null || !Array.isArray(matrix)) return [];
+  for(i = 0; i < matrix.length; i++){
+    if(i % 2 == 0) 
+    // fuction for sorting array
+      matrix[i].sort((a, b) => a - b)
+    else 
+        // fuction for reverse sorting array
+      matrix[i].sort((a, b) => a - b).reverse();
+  }
+  // return 1 dimensional array
+  return matrix.flat();
 }
